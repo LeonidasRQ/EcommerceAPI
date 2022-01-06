6f69961bf6ef216421c876a2f3bd6a7fcd193949
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-06 17:28:58 -0500

    implemented status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -23,18 +23,42 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
+  if (id == 999) {
+    res.status(404).json({
+      message: 'not found',
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000,
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
   });
 });
 
-router.post('', (req, res) => {
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
   const body = req.body;
   res.json({
     message: 'created',
     data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
