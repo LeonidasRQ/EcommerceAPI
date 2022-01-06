54a5e34bd5671802f40f49836b4419d34e54da57
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-06 15:49:24 -0500

    Refactored for distributing routing responsabilities

--- index.js
+++ index.js
@@ -1,64 +1,14 @@
 const express = require('express');
-const faker = require('faker');
-
+const routerApi = require('./routes');
 const app = express();
 const port = 3000;
+
+routerApi(app);
 
 app.get('/', (req, res) => {
   res.send('hello my server in express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
-});
-
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
 app.listen(port, () => {
-  console.log('Mi port' + port);
+  console.log('Mi port: ' + port);
 });
