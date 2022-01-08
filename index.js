935d662026cac48c7a686b5100b1367231c035b5
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-08 00:31:00 -0500

    Implemented error middlewares using boom library

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('hello my server in express');
