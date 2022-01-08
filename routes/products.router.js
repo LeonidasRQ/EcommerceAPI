473819dd0fc62320353986289554e318b8923a40
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-08 11:47:56 -0500

    Fixed minor issues

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const ProductService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('./../schema/product.schema');
 
 const router = express.Router();
 const service = new ProductService();
@@ -9,15 +15,19 @@ router.get('/', async (req, res) => {
   res.json(products);
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await service.findOne(id);
-    res.json(product);
-  } catch (error) {
-    next(error);
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 router.post('/', async (req, res) => {
   const body = req.body;
