e64c66a794de8d5ad8511c3c5ca6d2545401de58
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-10 11:32:06 -0500

    implemented database migrations

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -24,6 +24,11 @@ const UserSchema = {
     field: 'create_at',
     defaultValue: Sequelize.NOW,
   },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'customer',
+  },
 };
 
 class User extends Model {
