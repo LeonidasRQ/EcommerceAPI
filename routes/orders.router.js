36aab5c9eab347bdd494bb642b91421aeea12056
Leonidas <leonidas.ramirez.q@gmail.com>
2022-01-12 18:59:07 -0500

    implemented n:n relation between product and order entities

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const OrderService = require('../services/order.service');
 const validatorHandler = require('../middlewares/validator.handler');
-const { getOrderSchema, createOrderSchema } = require('../schema/order.schema');
+const {
+  getOrderSchema,
+  createOrderSchema,
+  addItemSchema,
+} = require('../schema/order.schema');
 
 const router = express.Router();
 const service = new OrderService();
@@ -28,6 +32,20 @@ router.post(
       const body = req.body;
       const newOrder = await service.create(body);
       res.status(201).json(newOrder);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/add-item',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newItem = await service.addItem(body);
+      res.status(201).json(newItem);
     } catch (error) {
       next(error);
     }
